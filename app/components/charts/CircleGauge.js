089b54e23d4035420a9f8f587fc6b887543bd7c9
Med-Amine-Fredj <medamin.fredj@gmail.com>
2022-06-01 16:41:08 +0100

    Update CircleGauge.js
    add shadow

--- app/components/charts/CircleGauge.js
+++ app/components/charts/CircleGauge.js
@@ -9,6 +9,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 import colors from '../../config/colors';
 import NoDataFound from '../NoDataFound';
+import { schedulePushNotification } from '../../../Helpers/notification';
 
 export default function CircleGauge({ value, min, max, warning, size }) {
   var levelStyle = StyleSheet.flatten([
@@ -47,6 +48,17 @@ export default function CircleGauge({ value, min, max, warning, size }) {
           : colors.light_green,
     },
   ]);
+
+  // const sendNotif = async (title, body) => {
+  //   await schedulePushNotification(title, body);
+  // };
+  // if (value == warning) {
+  //   sendNotif('Warning at the circle gauge', 'value is now in warning zone');
+  // } else if (value <= min) {
+  //   sendNotif('Warning at the circle gauge', 'value is below the minimum');
+  // } else if (value >= max) {
+  //   sendNotif('Warning at the circle gauge', 'value is over the maximum');
+  // }
 
   return (
     <>
@@ -102,6 +114,18 @@ export default function CircleGauge({ value, min, max, warning, size }) {
               lineCap="round"
               duration={1000}
               rotation={value < 0 ? (360 * value) / 100 : 0}
+              childrenContainerStyle={{
+                shadowColor:
+                  value <= min || value >= max
+                    ? colors.dark_red
+                    : value == warning
+                    ? colors.dark_warning
+                    : colors.light_green,
+                shadowOpacity: 0.4,
+                shadowRadius: 10,
+                elevation: 20,
+                backgroundColor: 'white',
+              }}
             >
               {(fill) => (
                 <>
