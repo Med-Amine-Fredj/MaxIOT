57c1256d50d99ff4c51ce2f5c36077e81c55285a
Med-Amine-Fredj <medamin.fredj@gmail.com>
2022-06-01 16:42:13 +0100

    Update Header.js
    add green and red dot for sync/async data

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,22 +11,38 @@ import {
 import Icon from './Icon';
 
 import colors from '../config/colors';
+import GreenDot from './GreenDot';
+import RedDot from './RedDot';
 
 let screenHeigth = Dimensions.get('window').height;
 
-function Header({ onPress, title }) {
+function Header({ onPress, title, realTime, onRealTimePress }) {
   return (
     <>
       <View style={styles.screen}>
-        <TouchableWithoutFeedback onPress={onPress}>
-          <View style={styles.iconContainer}>
-            <Icon iconName="left" iconColor={colors.primary} />
-          </View>
-        </TouchableWithoutFeedback>
+        <View style={{ flex: 1 }}>
+          <TouchableWithoutFeedback onPress={onPress}>
+            <View>
+              <Icon iconName="left" iconColor={colors.primary} />
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
         <View style={styles.titleContainer}>
           <Text style={styles.title} numberOfLines={1}>
             {title}
           </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <TouchableWithoutFeedback onPress={onRealTimePress}>
+            <View
+              style={{
+                flex: 1,
+              }}
+            >
+              <GreenDot visible={realTime} />
+              <RedDot visible={!realTime} />
+            </View>
+          </TouchableWithoutFeedback>
         </View>
       </View>
     </>
@@ -40,11 +56,13 @@ const styles = StyleSheet.create({
     height: screenHeigth * 0.06,
     // marginTop: Constants.statusBarHeight,
     alignItems: 'center',
+    justifyContent: 'space-evenly',
     borderBottomWidth: 0.5,
     borderBottomColor: 'gray',
   },
   titleContainer: {
     alignSelf: 'center',
+    justifyContent: 'center',
     flex: 2,
   },
   title: {
